e570abbaa3f28f31a88103cbbd68d036e3129641
Danilo-Roch <165158016+Danilo-Roch@users.noreply.github.com>
2024-03-27 10:39:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ alert('Bem vindo ao jogo do número secreto');
 
 let numeroSecreto = math.floor(math.radom() * 5);
 alert(`O número secreto é ${numeroSecreto}`);
+let tentativas = 3;
 
+while (tentativas > 0){
 let numeroDigitado = parseInt(prompt('Digite um número de 1 a 5'));
-
 if(numeroDigitado === numeroSecreto){
     alert('Parabéns, você acertou!');
+    break; 
 } else if(numeroDigitado > numeroSecreto){
     alert('Você digitou um número maior que o número secreto');
 } else {
     alert('Você digitou um número menor que o número secreto');
 }
+    tentativas = tentativas -1;
+}
+
+
