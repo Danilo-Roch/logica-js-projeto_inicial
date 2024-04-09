923a42f240533d2e4d607ab699fbbce1f54a9b68
Danilo-Roch <165158016+Danilo-Roch@users.noreply.github.com>
2024-04-09 08:19:21 -0300

    Create maioridade.js

---
+++ maioridade.js
@@ -0,0 +1,9 @@
+let maioridade = 18;
+let nome = prompt('Digite seu nome');
+let idade = parseInt(prompt('Digite sua idade'));
+
+if (idade >= maioridade) {
+    alert('Você atingiu a maioridade')
+} else {
+    alert('Você ainda é uma infanto juvenil')
+}
