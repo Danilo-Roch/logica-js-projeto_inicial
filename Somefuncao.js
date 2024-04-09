4fc741d1b42dfaaa0ae1c266bd7cfceefb5802a0
Danilo-Roch <165158016+Danilo-Roch@users.noreply.github.com>
2024-04-09 08:19:41 -0300

    Create Somefuncao.js

---
+++ Somefuncao.js
@@ -0,0 +1,13 @@
+function somaNumeros(a, b){
+    let soma = a + b;
+    alert(`A soma dos valores ${a} e ${b} é ${soma}`);
+}
+
+let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
+let segundoNumero = parseInt(prompt('Digite o segundo número'));
+
+somaNumeros(primeiroNumero, segundoNumero);
+
+if(2 == 2 and 3 == 3)
+if(2 == 2 or 3 == 3)
+
